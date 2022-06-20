5f9e18d09aaf5a16d1cd1e5f1ede9847611ade9d
Pakanate <620108020002@dpu.ac.th>
2022-06-20 14:01:19 +0700

    132

---
+++ oop2/modulepackage/my_math_use.py
@@ -0,0 +1,11 @@
+#from my_math import square,circle_area
+
+import my_math as my
+
+if __name__ == '__main__':
+
+    square_num = square(5)
+    print(square_num)
+    print(IP)
+    print(circle_area(3))
+    
